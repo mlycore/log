246956587513965fe0cc9800b246fde7f54fda34
mlycore <maxwell92@126.com>
2024-03-07 14:11:28 +0800

    chore: add license header
    Signed-off-by: mlycore <maxwell92@126.com>

--- formatter.go
+++ formatter.go
@@ -1,3 +1,17 @@
+// Copyright 2024 mlycore. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package log
 
 import (
@@ -5,10 +19,10 @@ import (
 	"fmt"
 )
 
-//Formatter will decide how logs are printed
-//Default consist of:
-//* TextFormatter, print as "deployment=kubestar namespace=default msg=deployment not found"
-//* JSONFormatter, print as "{"deployment": "kubestar", "namespace": "default", "msg": "deployment not found"}"
+// Formatter will decide how logs are printed
+// Default consist of:
+// * TextFormatter, print as "deployment=kubestar namespace=default msg=deployment not found"
+// * JSONFormatter, print as "{"deployment": "kubestar", "namespace": "default", "msg": "deployment not found"}"
 type Formatter interface {
 	Print(fields Fields, ctx Context) string
 	SetColor(color bool)
